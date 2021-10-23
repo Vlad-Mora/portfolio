015ac2653503c5b75233c64c535a14a425e23c7d
Kristler <kristler@dmginc.gg>
2021-10-23 10:16:14 +0100

    [+] SimpleArraySum

--- src/components/organisms/SimpleArraySum/SimpleArraySum.tsx
+++ src/components/organisms/SimpleArraySum/SimpleArraySum.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import Button from 'src/components/atoms/Button/Button';
+import InputField from 'src/components/atoms/InputField';
+import ProjectLayout from 'src/components/templates/ProjectLayout/ProjectLayout';
 
 interface ISimpleArraySumProperties {}
 
 const SimpleArraySum: React.FC<ISimpleArraySumProperties> = ({  }) => {
 
+    const [output, setOutput] = React.useState<number>();
+    const [array, setArray] = React.useState<string>();
+
+    function handleClick() {
+        let total = 0;
+        array?.replace(" ", "").split(",").forEach(number => {
+            total += parseInt(number);
+        });
+        setOutput(total);
+    };
+
     return (
-        <>
-        </>
+        <ProjectLayout
+            title="SimpleArraySum"
+            description="
+                Input an list of numbers, then return the sum of them.
+            "
+            input={
+                <>
+                    <InputField
+                        placeHolder="E.g 1, 2, 3, 4"
+                        setInput={setArray}
+                        isInputNumber={false}
+                    />
+                </>
+            }
+            output={output}
+            button={
+                <>
+                    <Button
+                        text="Add"
+                        onClick={() => {
+                            (array !== undefined) &&
+                            handleClick();
+                        }}
+                    />
+                </>
+            }
+        />
     );
 };
 
