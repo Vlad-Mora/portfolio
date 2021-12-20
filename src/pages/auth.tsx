93b3b412151edd4447ae4403c65d414c1c2ee91c
Kristler <kristler@dmginc.gg>
2021-12-20 20:32:10 +0000

    [Spotify Clone]: Continuing work on access code

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import { useRouter } from "next/router";
 import axios from "axios";
+import queryString from "query-string";
 import { ContextContainer, ContextProps } from "@context/ContextContainer";
 
 const AuthPage = () => {
@@ -18,20 +19,30 @@ const AuthPage = () => {
     const error = new URLSearchParams(window.location.search).get("error");
     if (error === null) {
       const code = new URLSearchParams(window.location.search).get("code");
-
+      console.log(code);
+      console.log(
+        queryString.stringify({
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: "https://vlad-mora-portofolio.herokuapp.com/auth/",
+        })
+      );
       axios
-        .post(`https://accounts.spotify.com/api/token`, {
-          headers: {
-            Authorization:
-              "Basic MTFjMmIzY2Y3NTBjNDc0YzhkZjZlZDExOGY0OTdmOGE6NTI4ZDU1ZTM2ZDc3NGFmYThmNTIxNDJiODA3OGJmYWI",
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          data: {
+        .post(
+          `https://accounts.spotify.com/api/token`,
+          queryString.stringify({
             grant_type: "authorization_code",
             code: code,
             redirect_uri: "https://vlad-mora-portofolio.herokuapp.com/auth/",
-          },
-        })
+          }),
+          {
+            headers: {
+              Authorization:
+                "Basic MTFjMmIzY2Y3NTBjNDc0YzhkZjZlZDExOGY0OTdmOGE6NTI4ZDU1ZTM2ZDc3NGFmYThmNTIxNDJiODA3OGJmYWI",
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        )
         .then((response) => {
           console.log(response);
         })
