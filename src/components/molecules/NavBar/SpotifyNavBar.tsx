fafebb3e74c6a55304df649b26fdcffe99955e5e
Kristler <kristler@dmginc.gg>
2022-01-01 09:55:52 +0000

    [Spotify Clone]: Created and styled user profile menu

--- src/components/molecules/NavBar/SpotifyNavBar.tsx
+++ src/components/molecules/NavBar/SpotifyNavBar.tsx
@@ -9,17 +9,20 @@ interface ISpotifyNavBarProps {
 
 const SpotifyNavBar: React.FC<ISpotifyNavBarProps> = ({ name, images }) => {
 
+    const [menuActive, setMenuActive] = React.useState<boolean>(true);
+
     return (
         <div className="spotify-navbar">
-            <div className="userTag-wrapper moving-glow">
-                {images && images[0] ?
-                    <div className="userTag-image" style={{ "--pfpHeight": `${images[0].height}px`, "--pfpWidth": `${images[0].width}px`} as CSSProperties}>
-                       <img src={`${images[0].url}`}/>
-                   </div> 
-                :
-                    <i className="far fa-user"/>
-                }
-                <div className="userTag-name noselect">{name}</div>
+            <div className="userTag-wrapper moving-glow" onClick={() => setMenuActive(!menuActive)}>
+                <div className="image">
+                    {images && images[0] 
+                    ? <img src={`${images[0].url}`} style={{ "--pfpHeight": `${images[0].height}px`, "--pfpWidth": `${images[0].width}px`} as CSSProperties}/> 
+                    : <i className="far fa-user"/>
+                    }
+                </div>
+                <div className={`menu-wrapper ${menuActive}`}>
+                    <div className="name">{name}</div>
+                </div>
             </div>
         </div>
     );
